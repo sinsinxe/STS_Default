56f1508320fdaec4a24dfec1140963c151d16d96
sinsinxe <sinsinxe@gmail.com>
2025-09-28 17:23:38 +0900

    저장된 로컬 변수 초기화 툴 추가

---
+++ Tools/Editor/STS_ClearPlayerPrefs.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+public static class STS_ClearPlayerPrefs
+{
+	[MenuItem("Tools/[STS] Clear PlayerPrefs")]
+	public static void ClearPlayerPrefs()
+	{
+		PlayerPrefs.DeleteAll();
+		PlayerPrefs.Save();
+		Debug.Log("✅ PlayerPrefs 모두 초기화 완료");
+	}
+}
+#endif
