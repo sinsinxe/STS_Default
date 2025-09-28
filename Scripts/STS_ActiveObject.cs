3274a3477c2fa59e8552fd9259306df279e80eb4
sinsinxe <sinsinxe@gmail.com>
2025-09-28 14:24:31 +0900

    공용 스크립트 추가

---
+++ Scripts/STS_ActiveObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class STS_ActiveObject : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+	public GameObject ActiveObject;
+	
+	void Start()
+    {
+	   
+    }
+    
+	public void SetActiveTrue()
+	{
+		ActiveObject.SetActive(true);
+	}
+    
+	public void SetActiveFalse()
+	{
+		ActiveObject.SetActive(false);
+	}
+    
+
+}
